a0d5995a4cd5ab105aa4cf934bac1c32a9e28dfc
Miika Tuominen <miika.km.tuominen@gmail.com>
2026-02-11 22:39:54 +0200

    remove delete from sync script

--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-rsync -viah --info=progress2 --stats --delete --delete-excluded --exclude='.git' ./ pi:quesys/
+#rsync -viah --info=progress2 --stats --delete --delete-excluded --exclude='.git' ./ pi:quesys/
+# DONT DELETE THE DATABASE!!
+rsync -viah --info=progress2 --stats --exclude='.git' ./ pi:quesys/
